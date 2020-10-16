3c0fed81b3028f089ea38eb9643fb01a76ada86f
Joeway <joewaybanh@hotmail.com>
2020-10-16 10:18:43 +0200

    finished the first 3 exercises of Step 2 - JS algorithms

--- algo/algo1.js
+++ algo/algo1.js
@@ -1,16 +1,16 @@
 /*
-Write a function which, given two angles from a triangle, will return the value of the third one.
-(Reminder: the sum of the values of the three angles in a triangle is **always** 180°)
+Write a function which, given two angles from a triangle,
+will return the value of the third one.
+(Reminder: the sum of the values of the three angles
+in a triangle is **always** 180°)
 Ex: 
 thirdAngle(90, 30) should return 60
 thirdAngle(20, 80) should return 80
 */
 
-// function thirdAngle(a, b) {
-//   // Your code here !
-// }
+function thirdAngle(a, b) {
+  return 180 - (a + b)
+}
 
-// console.log(thirdAngle(90, 30));
-// console.log(thirdAngle(20, 80));
-let count = {a:1,b:2}
-console.log(count['a'])
+console.log(thirdAngle(90, 30));
+console.log(thirdAngle(20, 80));
